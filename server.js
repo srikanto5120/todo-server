e4bfebd8dc1a50e9fb9f31be837a28a037efae1c
srikanto5120 <shreekantaray@gmail.com>
2022-12-23 22:46:26 +0600

    make it better

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
-const cors= require("cors")
+const cors = require("cors");
 
 // Bind the router db to the app
 server.db = router.db;
 server.use(middlewares);
-server.use(cors())
+server.use(
+  cors({
+    origin: "https://srikanto.netlify.app",
+
+    methods: ["GET", "POST", "DELETE"],
+  })
+);
 server.use(router);
 
 server.listen(port);
